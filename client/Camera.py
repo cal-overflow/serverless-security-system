53611afe8eb30f1dbf7ab5f560f7fd0b8511ac9d
cal <42817415+cal-overflow@users.noreply.github.com>
2022-12-23 14:05:59 -0600

    Move video processing to client (#26)

--- client/Camera.py
+++ client/Camera.py
@@ -1,78 +1,125 @@
 import cv2 as cv
 import time
 import os
+import logging
 from dotenv import load_dotenv
 import uuid
 from fileservices import create_folder
 
+
 load_dotenv()
 output_path = os.getenv('OUTPUT_PATH', './tmp')
+WIDTH = int(os.getenv('WIDTH', '0'))
+HEIGHT = int(os.getenv('HEIGHT', '0'))
+MOTION_THRESHOLD = int(os.getenv('MOTION_THRESHOLD', 3000))
+
+
+def get_contours_between_frames(frame1, frame2):
+    '''Returns a list of contours between the two given frames'''
+
+    if frame1 is None or frame2 is None:
+        return []
+
+    difference = cv.absdiff(frame1, frame2)
+    gray_difference = cv.cvtColor(difference, cv.COLOR_BGR2GRAY)
+    blur = cv.GaussianBlur(gray_difference, (5, 5), 0)
+    _, thresh = cv.threshold(blur, 20, 255, cv.THRESH_BINARY)
+    dilated = cv.dilate(thresh, None, iterations=3)
+    contours, _ = cv.findContours(dilated, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    return contours
+
 
 
 class Camera:
-    def __init__(self, camera_name, clip_length):
-        self.name = camera_name
-        self.clip_length = clip_length
+    def __init__(self):
         self.camera = None
-        self.width = None
-        self.height = None
+        self.width = WIDTH
+        self.height = HEIGHT
         self.fps = None
 
 
-    def calibrate_camera(self):
+    def calibrate(self):
         '''Calibrate the camera. Get the true FPS (including processing) from the camera'''
 
-        print('Calibrating camera...')
+        logging.info('Calibrating camera...')
         self.camera = cv.VideoCapture(0)
 
         if not self.camera.isOpened():
             # Attempt to open capture device once more, after a failure
             self.camera.open()
             if not self.camera.isOpened():
-                print('Unable to open camera')
+                logging.error('Unable to open camera')
                 exit(1)
 
         start_time = time.time()
         count = 0
         while int(time.time() - start_time) < 10:
-            ret, frame = self.camera.read()
+            _, _ = self.camera.read()
             count += 1 # number of frames
 
-        self.width = int(self.camera.get(cv.CAP_PROP_FRAME_WIDTH))
-        self.height = int(self.camera.get(cv.CAP_PROP_FRAME_HEIGHT))
-        self.fps = int(count / 10)
+        if self.width == 0 or self.height == 0:
+            self.width = int(self.camera.get(cv.CAP_PROP_FRAME_WIDTH))
+            self.height = int(self.camera.get(cv.CAP_PROP_FRAME_HEIGHT))
 
-        print(f'Camera calibration complete.\nDimensions: {self.width}x{self.height}\nFPS: {self.fps}')
+        self.camera.set(cv.CAP_PROP_FRAME_WIDTH, self.width)
+        self.camera.set(cv.CAP_PROP_FRAME_HEIGHT, self.height)
+        self.fps = int(count / 10)
+        self.camera.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc(*'MJPG')) # this fourcc allows for faster processing - https://stackoverflow.com/a/74234176
+
+        logging.info(f'Camera calibration complete - {self.width}x{self.height} at {self.fps} fps')
         return
 
 
-    def record_clip(self):
+    def record_clip(self, clip_length, is_motion_outlined):
         '''Record a video clip. Will calibrate the camera if it has not already been calibrated.'''
         if self.camera is None:
-            self.calibrate_camera()
+            self.calibrate()
 
         start_time = time.time()
-        filename = f"{output_path}/{time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime(start_time))}_{self.name}.mp4"
-        print('Recording clip')
+        filename = f"{output_path}/{time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime(start_time))}.mp4"
        
-        # Figure out if this video encoding is sufficient (I want it to work on all devices without issues)
-        # This video encoding might be what I'm looking for. It looks like it is creating mp4 videos that are playing in chrome well. TODO - tesst in other browsers
         fourcc = cv.VideoWriter_fourcc(*'avc1')
         video_writer = cv.VideoWriter(filename, fourcc, self.fps, (self.width, self.height))
 
 
-        for i in range(self.fps * self.clip_length):
-            ret, frame = self.camera.read() # read frame from camera
+        prev_frame = None
+        contains_motion = False
 
+        for i in range(self.fps * clip_length):
+            _, frame = self.camera.read() # read frame from camera
+            frame_copy = frame.copy() # Copy the previous frame before the timestamp or motion-outline boxes are drawn
             timestamp = time.strftime("%m/%d/%Y %I:%M:%S %p", time.localtime(time.time()))
-            
-            # draw the text twice to give "outline" effect (ensure the text is visible regardless of frame)
+
+
+            if prev_frame is not None:
+                contours = get_contours_between_frames(frame, prev_frame)
+
+                for contour in contours:
+                   if cv.contourArea(contour) >= MOTION_THRESHOLD:
+                       if not contains_motion:
+                           contains_motion = True
+                       if is_motion_outlined:
+                           (x, y, w, h) = cv.boundingRect(contour)
+                           cv.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 1)
+
+                       break
+
+            # Draw a timestamp on the frame
+            # draw twice to give "outline" effect (ensure the text is visible regardless of frame)
             frame = cv.putText(frame, timestamp, (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 4, cv.LINE_AA)
             frame = cv.putText(frame, timestamp, (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv.LINE_AA)
             video_writer.write(frame)
 
+            prev_frame = frame_copy
+
             cv.waitKey(1)
+
         video_writer.release()
+
+        motion_flag = "CONTAINS-MOTION" if contains_motion else "NO-MOTION"
+        updated_filename = f"{output_path}/{time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime(start_time))}_{motion_flag}.mp4"
+        os.rename(filename, updated_filename)
+
 
 
     def release(self):
--- client/main.py
+++ client/main.py
@@ -1,38 +1,58 @@
 import os
 from dotenv import load_dotenv
-import uuid
+import time
 import threading
-from fileservices import create_folder, upload_files
+from fileservices import create_folder, upload_files, get_and_parse_settings_file
 from Camera import Camera
+import logging
 
 
 load_dotenv()
-
 output_path = os.getenv('OUTPUT_PATH', './tmp')
-clips_per_upload = int(os.getenv('CLIPS_PER_UPLOAD', 50))
-clip_length = int(os.getenv('CLIP_LENGTH', 30))
-camera_name = os.getenv('CAMERA_NAME', f'CAMERA_{uuid.uuid4().hex[:8]}')
+
+logging.basicConfig(
+    format='[%(asctime)s] %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+
+
+
+def sync_configuration():
+    global settings
+    settings = get_and_parse_settings_file()
+
+    global time_since_last_config_sync
+    time_since_last_config_sync = time.time()
+
+    logging.info('Configuration synced')
 
 
 if __name__ == '__main__':
-    print(f'Output configuration:\nClips per upload: {clips_per_upload}\nClip length: {clip_length}s\nCamera name: {camera_name}')
-    camera = Camera(camera_name, clip_length)
+    sync_configuration()
 
-    video_writer = None
+    camera = Camera()
+    camera.calibrate()
 
     create_folder(output_path) 
 
     try:
         clips_since_last_upload = 0
         uploading_thread = None
+
+        logging.info('Beginning footage capture')
         while True:
-            camera.record_clip()
+            if time_since_last_config_sync < time.time() - 3600:
+                settings_sync_thread = threading.Thread(target=sync_configuration, name="SettingsSyncer", args=())
+                settings_sync_thread.start()
+
+            camera.record_clip(settings['clip_length'], settings['is_motion_outlined'])
             clips_since_last_upload += 1
             
-            if clips_since_last_upload >= clips_per_upload:
+            if clips_since_last_upload >= settings['clips_per_upload']:
                 if uploading_thread is not None:
                     if uploading_thread.is_alive():
-                        print('Thread still running in background')
                         continue
                     
                 uploading_thread = threading.Thread(target=upload_files, name="FileUploader", args=(output_path,))
@@ -40,8 +60,6 @@ if __name__ == '__main__':
                 clips_since_last_upload = 0
             
     except Exception as e:
-        print(f'An error occurred: {e}')
-        if video_writer is not None:
-            video_writer.release()
+        logging.error(f'An error occurred: {e}')
     camera.release()
 
