7715065ed111369d53ccff44ef738cc0781c931a
Christian Lisle <lisleachristian@gmail.com>
2022-12-23 23:07:38 -0600

    Refactor api lamnbda into single file (for now) and add /configuration routes

--- cloud/lambda/api.py
+++ cloud/lambda/api.py
@@ -5,11 +5,13 @@ import datetime
 import time
 
 
+FUNCTION_NAME = os.environ.get('FUNCTION_NAME')
 BUCKET = os.environ.get('S3_BUCKET')
+PRESIGN_URL_EXPIRATION_TIME = int(os.environ.get('PRESIGN_URL_EXPIRATION_TIME'))
+SETTINGS_FILE_KEY = "configuration/settings.json"
 today = datetime.date.today()
-presign_url_expiration_time = int(os.environ.get('PRESIGN_URL_EXPIRATION_TIME'))
 
-
+# Helper functions
 def get_files_in_folder(folder, suffix=''):
     s3_client = boto3.client('s3')
     files = []
@@ -22,13 +24,13 @@ def get_files_in_folder(folder, suffix=''):
             if not obj_data['Key'].endswith(suffix):
                 continue
 
-            presigned_url = s3_client.generate_presigned_url('get_object', Params={'Bucket': BUCKET, 'Key': obj_data['Key']}, ExpiresIn=presign_url_expiration_time)
+            presigned_url = s3_client.generate_presigned_url('get_object', Params={'Bucket': BUCKET, 'Key': obj_data['Key']}, ExpiresIn=PRESIGN_URL_EXPIRATION_TIME)
             
             filename = obj_data['Key'].split('/')[-1]
             files.append({
                 'filename': filename,
                 'video_url': presigned_url,
-                'expiration': time.time() + presign_url_expiration_time
+                'expiration': time.time() + PRESIGN_URL_EXPIRATION_TIME
             })
 
     return files
@@ -66,10 +68,7 @@ def get_videos_without_motion(date_folder, camera_filter):
     return videos
 
 
-# TODO - implement authentication! Auth should be required in order to do all requests
-
-
-# Handler (API) functions:
+# Video
 def get_videos(event, _):
 
     # default query parameters
@@ -101,8 +100,73 @@ def get_videos(event, _):
 
     return { 'statusCode': 200, 'body': json.dumps(videos) }
 
-# TODO:
-# def get_settings(event, _):
 
-# TODO:
-# def update_settings(event, _):
+# Configuration
+def get_configuration(event, _):
+    s3_client = boto3.client('s3')
+
+    s3_client.download_file(BUCKET, SETTINGS_FILE_KEY, '/tmp/settings.json')
+
+    with open('/tmp/settings.json') as settings_file:
+        settings = json.load(settings_file)
+
+    settings['presigned_url_expiration_time'] = PRESIGN_URL_EXPIRATION_TIME
+    
+    return { 'statusCode': 200, 'body': json.dumps(settings) }
+
+
+def update_configuration(event, _):
+    new_configuration = json.loads(event['body'])
+
+    # Validate the new config
+    if type(new_configuration['is_motion_outlined']) != bool:
+        return { 'statusCode': 400 }
+    if type(new_configuration['clip_length']) != int:
+        return { 'statusCode': 400 }
+    if type(new_configuration['clips_per_upload']) != int:
+        return { 'statusCode': 400 }
+    if type(new_configuration['presign_url_expiration_time']) != int:
+        return { 'statusCode': 400 }
+
+
+    s3_client = boto3.client('s3')
+    lambda_client = boto3.client('lambda')
+
+    lambda_client.update_function_configuration(
+            FunctionName=FUNCTION_NAME,
+            Environment={
+                'Variables': {
+                    'FUNCTION_NAME': FUNCTION_NAME,
+                    'S3_BUCKET': BUCKET,
+                    'PRESIGN_URL_EXPIRATION_TIME': str(new_configuration['presign_url_expiration_time']),
+                }
+            }
+        )
+    
+    del new_configuration['presign_url_expiration_time']
+
+    with open('/tmp/output.json', 'w') as output_file:
+        json.dump(new_configuration, output_file)
+
+    s3_client.upload_file('/tmp/output.json', BUCKET, SETTINGS_FILE_KEY)
+
+    return { 'statusCode': 201, 'body': event['body'] }
+
+
+
+# TODO - implement authentication - Auth should be required in order to do all requests
+def handler(event, context):
+    print(event)
+
+    print(event['path'])
+    print(event['httpMethod'])
+
+    if event['path'].startswith('/videos'):
+        if event['httpMethod'] == 'GET':
+            return get_videos(event, context)
+
+    elif event['path'].startswith('/configuration'):
+        if event['httpMethod'] == 'GET':
+            return get_configuration(event, context)
+        if event['httpMethod'] == 'POST':
+            return update_configuration(event, context)
