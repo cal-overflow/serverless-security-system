f2a6bb02020984ebe7eb1e51a1e3a482509e9fbf
cal <42817415+cal-overflow@users.noreply.github.com>
2022-12-31 10:53:47 -0600

    Fix auto delete issues (#41)

--- cloud/lambda/api/config.py
+++ cloud/lambda/api/config.py
@@ -75,16 +75,19 @@ def update_config(event, _):
 
 
     if new_configuration['days_to_keep_motionless_videos'] < old_configuration['days_to_keep_motionless_videos']:
-        delete_old_footage_response = lambda_client.invoke(
+        lambda_response = lambda_client.invoke(
             FunctionName=VIDEO_PURGER_FUNCTION_NAME,
             Payload=json.dumps({
                 'previous_config': old_configuration,
                 'new_config': new_configuration
             }),
         )
+        lambda_response_payload = json.loads(lambda_response['Payload'].read())
 
-        if delete_old_footage_response['statusCode'] != 200:
+        if lambda_response_payload['statusCode'] != 200:
             return { 'statusCode': 500, 'body': json.dumps('Something went wrong when updating the `days_to_keep_motionless_videos`. The configuration failed to update.') }
+    elif new_configuration['days_to_keep_motionless_videos'] > 365:
+        return { 'statusCode': 400, 'body': json.dumps('days_to_keep_motionless_videos should not be greater than 365. The configuration failed to update.') }
     
     with open('/tmp/output.json', 'w') as output_file:
         json.dump(new_configuration, output_file)
--- cloud/lambda/video_purger/index.py
+++ cloud/lambda/video_purger/index.py
@@ -10,17 +10,34 @@ SETTINGS_FILE_KEY = "configuration/settings.json"
 today = datetime.today()
 s3_client = boto3.client('s3')
 
+
 def config_update_handler(previous_config, new_config):
     '''Handle scenarios where the configuration is updated. This is invoked by the API when a user updates the days_to_keep_motionless_videos option'''
 
     folders_to_remove = []
     for n in list(range(new_config['days_to_keep_motionless_videos'], previous_config['days_to_keep_motionless_videos'] + 1)):
         day_to_remove = today - timedelta(days=n)
-        folder = f'footage/normal/{day_to_remove.strftime("%Y-%m/%d")}'
+        month_folder = f'footage/normal/{day_to_remove.strftime("%Y-%m")}'
+        folder = f'{month_folder}/{day_to_remove.strftime("%d")}'
         folders_to_remove.append(folder)
 
-    for folder in folders_to_remove:
+        paginator = s3_client.get_paginator('list_objects_v2')
+        pages = paginator.paginate(Bucket=BUCKET, Prefix=folder)
+
+        for page in pages:
+            if 'Contents' not in page.keys():
+                continue
+
+            for obj_data in page['Contents']:
+                print(f"deleting: {obj_data['Key']}")
+                s3_client.delete_object(Bucket=BUCKET, Key=obj_data['Key'])
+        print(f'deleting: {folder}')
         s3_client.delete_object(Bucket=BUCKET, Key=folder)
+
+        res = s3_client.list_objects_v2(Bucket=BUCKET, Prefix=month_folder)
+        if res['KeyCount'] == 1:
+            print(f'deleting: {month_folder}')
+            s3_client.delete_object(Bucket=BUCKET, Key=month_folder)
 
 
     return { 'statusCode': 200 }
@@ -36,7 +53,24 @@ def handler(event, _):
         config = json.load(settings_file)
 
     cutoff_date = today - timedelta(days=(config['days_to_keep_motionless_videos'] + 1))
-    folder_to_delete = f'footage/normal/{cutoff_date.strftime("%Y-%m/%d")}'
+    month_folder = f'footage/normal/{cutoff_date.strftime("%Y-%m")}'
+    folder_to_delete = f'{month_folder}/{cutoff_date.strftime("%d")}'
 
+
+    paginator = s3_client.get_paginator('list_objects_v2')
+    pages = paginator.paginate(Bucket=BUCKET, Prefix=folder_to_delete)
+
+    for page in pages:
+        if 'Contents' not in page.keys():
+            continue
+
+        for obj_data in page['Contents']:
+            print(f"deleting: {obj_data['Key']}")
+            s3_client.delete_object(Bucket=BUCKET, Key=obj_data['Key'])
+    print(f"deleting: {folder_to_delete}")
     s3_client.delete_object(Bucket=BUCKET, Key=folder_to_delete)
 
+    res = s3_client.list_objects_v2(Bucket=BUCKET, Prefix=month_folder)
+    if res['KeyCount'] == 1:
+        print(f"deleting: {month_folder}")
+        s3_client.delete_object(Bucket=BUCKET, Key=month_folder)
