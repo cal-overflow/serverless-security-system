b4f684b4f5d3243361253ec358282018a72136b9
Christian Lisle <lisleachristian@gmail.com>
2023-01-16 20:12:09 -0600

    update gateway (login/logout) pages

--- cloud/ui/pages/login.vue
+++ cloud/ui/pages/login.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="mx-auto flex h-screen">
-    <div class="max-w-screen-sm m-auto w-full bg-card-light dark:bg-card-dark p-4 flex flex-wrap shadow-lg dark:shadow-shadow-dark hover:shadow-none hover:rounded motion-safe:animate-fade-in-fast transition">
-      <p class="text-2xl font-bold">Login</p>
+    <div class="max-w-screen-sm m-auto w-full sm:bg-card-light sm:dark:bg-card-dark p-8 sm:p-4 flex flex-wrap sm:shadow-lg sm:dark:shadow-shadow-dark sm:hover:shadow-none sm:hover:rounded motion-safe:animate-fade-in-fast transition">
+      <p class="text-3xl font-bold">Login</p>
       <form @submit.prevent="submitLogin" class="flex flex-wrap w-full">
-        <label for="username">User</label>
+        <label for="username" class="text-lg">User</label>
         <input
           v-model="username"
           type="text"
@@ -14,7 +14,7 @@
           class="w-full resize-none px-4 py-2 bg-extra-gray-light dark:bg-extra-gray-dark rounded-lg outline-none focus:rounded-sm focus:ring focus:ring-primary-light focus:dark:ring-primary-dark transition"
         />
 
-        <label for="password">Pin</label>
+        <label for="password" class="text-lg">Pin</label>
         <input
           v-model="password"
           type="password"
--- cloud/ui/pages/logout.vue
+++ cloud/ui/pages/logout.vue
@@ -1,6 +1,34 @@
 <template>
-  <div>
-    <p class="text-4xl font-bold">Signing you out...</p>
+  <div class="mx-auto flex h-screen">
+    <div class="max-w-screen-sm m-auto w-full sm:bg-card-light sm:dark:bg-card-dark p-8 sm:p-4 flex flex-wrap sm:shadow-lg sm:dark:shadow-shadow-dark sm:hover:shadow-none sm:hover:rounded motion-safe:animate-fade-in-fast transition animate-pulse">
+      <p class="text-3xl font-bold">Signing you out...</p>
+
+      <form class="flex flex-wrap w-full">
+        <label for="lazyUsername" class="text-lg">User</label>
+        <input
+          name="lazyUsername"
+          disabled
+          class="w-full resize-none px-4 py-2 bg-extra-gray-light dark:bg-extra-gray-dark rounded-lg outline-none focus:rounded-sm focus:ring focus:ring-primary-light focus:dark:ring-primary-dark transition"
+        />
+
+        <label for="lazyPassword" class="text-lg">Pin</label>
+        <input
+          name="lazyPassword"
+          disabled
+          class="w-full resize-none px-4 py-2 bg-extra-gray-light dark:bg-extra-gray-dark rounded-lg outline-none focus:rounded-sm focus:ring focus:ring-primary-light focus:dark:ring-primary-dark transition"
+        />
+
+        <div class="flex justify-between w-full">
+          <p class="text-sm my-auto"></p>
+          <input
+            type="submit"
+            value="Login"
+            disabled
+            class="rounded-md py-1 px-2 my-4 bg-primary-light dark:bg-primary-dark text-white transition opacity-25 cursor-default"
+          />
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
