28e4218bcc9320cfc058d8b856126e95c9db7f10
Christian Lisle <lisleachristian@gmail.com>
2023-03-05 00:03:56 -0600

    Add dynamic favicon

--- cloud/ui/nuxt.config.js
+++ cloud/ui/nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Poppins:400,700' }
     ],
   },
